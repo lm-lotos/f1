1093ba96ac6dcc7972c42d7ffd6785a626930cbe
lm-lotos <lm-lotos@ukr.net>
2025-10-28 21:40:04 +0100

    без номерів 16 та 17

---
+++ lesson_27_10.py
@@ -0,0 +1,154 @@
+
+# # для заданної кількості ітерацій
+# for i in щось:
+#     дія
+
+
+# # поки умова виконується
+# while умова:
+#     дія
+
+
+
+# # #ax ^ 2 + bx + c = 0
+
+# # a = int(input("a: "))
+# # b = int(input("b: "))
+# # c = int(input("c: "))
+
+# # print(f"Дано квадратне рівняння")
+
+# # d = b **2 - 4 8 a * c
+
+# # if d < 0:
+# #     print("Коренів нема")
+# # elif d == 0:
+# #     x = (-b) / (2 * a)
+# #     print ("Корінь один: x =", x)
+# # else:
+# #     x1 = (-b - d ** 0.5) / (2* a)
+# #     x2 = (-b - d ** 0.5) / (2* a)
+# #   print(f"x1 = {x1}, x2 = {x2}")
+
+
+# # ----------------------------------
+
+# # for | whil
+
+# # n = int(input("Enter number: "))
+# # # i = "qwerty"
+# # #print (i)
+# # for i in range(n):
+# #     print("Hello, Python")
+# #     print(i)
+
+# #  ----------------------------------
+
+# #2  ------ якщо if a > b: (то це буде "bag" - помилка)
+
+# # a = int(input("a: "))
+# # b = int(input("b: "))
+
+# # if a < b:
+# #     a, b = b, a
+
+# # for i in range(a, b +1):
+# #     print(i)
+
+# # ----------------------------------
+
+# #3  
+
+# # n = int(input("Enter sec: "))
+
+# # for i in range(n, 0, -1):
+# #     print(i)
+# # print("Start!")
+
+# #  ------------------------------
+
+# #3
+# # n = int(print("Enter sec: "))
+
+# # for i in range(n, 0, -1):
+# #     print(i)
+
+# # while n > 0:
+# #     print(n)
+# #     n -= 1 # n = n - 1
+
+# # print("Start!")
+
+# #---------------------------
+
+# #8 Кількість сходинок.
+
+# Вхідні дані:
+
+#  4
+
+# Вихідні дані:
+
+# # n = int(input("Count: "))
+
+# # for el in range(1, n+1):
+# #     print("#" * el)
+
+# # while n > 0:
+# #     print("#" * n)
+# #     n -= 1 # n = n - 1
+
+
+# # ---------------------------------------
+
+
+# #9. За даним цілим додатнім числом n обчисліть n! значення факторіалу цього числа. Факторіал числа  (можна виконати як через for так і через while)
+
+# # n = int(input("Count: "))
+
+# # 5! = 1 * 2 * 3 * 4 * 5 -> range(1,b+1)
+# # 5! = 5 * 4 * 3 * 2 * 1 -> white n > 0: n -= 1
+
+# f = 1
+
+# for  i in rabge(1, n+1):
+#      prunt(i)
+#      f *= 1
+
+# print(f)
+
+# print("------------------------")
+
+
+# while n > 0:
+#     print(n)
+#     n -= 1
+#     f *= n
+
+# print(f)
+
+# def avarage():
+#    n = int(input("Count: "))
+#    f = 1
+#    while n > 0:
+#     print(n)
+#     n -= 1
+#     f *= n
+
+#    print(f)
+
+# avarage()
+# print(n)
+
+# def numeric()
+#    return
+# print(numeric())
+
+
+
+
+
+
+
+
+
